5d14840de742d8c520bcb0237f17a80897836833
gerardorosales2222 <gerardorosales2222@gmail.com>
2023-09-21 10:40:03 -0300

    Se realiza la conexión a la base de datos

--- src/modelos/ConexionInicial.java
+++ src/modelos/ConexionInicial.java
@@ -1,13 +1,15 @@
 package modelos;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class ConexionInicial {
     private Connection c;
     public Connection iniciarConexion(){
         try {
-            
-        } catch (Exception e) {
+            c = DriverManager.getConnection("jdbc:mysql://localhost/Biblioteca", "root", "");
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "No se puede conectar a la base de datos.");
         }
         return c;
